27aaa85b3f532e57a740bac10ffc4f2671094083
phrygius <jasong.extra@gmail.com>
2016-01-06 18:14:20 -0500

    CSS animations for view swapping, better JS, non-fixed background container

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -22,7 +22,7 @@ function makeTranslate3d(ele, x, y, z) {
   }
   ele.style.transform = 'translate3d(' + x + 'px, ' + y + 'px, ' + z + 'px)';
 }
-function onScroll(container, targetEle) {
+function makeScrollTransformForElement(container, targetEle, factor) {
   return debounce(function(event) {
     scrollTop = container.scrollTop;
 
@@ -32,45 +32,40 @@ function onScroll(container, targetEle) {
       $('body').removeClass('scrolling');
     }, 1000);
 
-    makeTranslate3d(targetEle, 0, (0 - Math.floor(scrollTop / 10)), 0);
+    makeTranslate3d(targetEle, 0, (0 - Math.floor(scrollTop / factor)), 0);
   }, 10);
 }
 /**
  * [left ][center][right]
  */
-var viewportContainer = document.getElementById('vpc');
-var viewports = {
-  left: {
-    querySelector: '.viewport.left',
-    element: document.querySelector('.viewport.left'),
-    style: "translate3d(0vw, 0vh, 0px)"
-  },
-  center: {
-    querySelector: '.viewport.center',
-    element: document.querySelector('.viewport.center'),
-    style: "translate3d(-100vw, 0vh, 0px)"
-  },
-  right: {
-    querySelector: '.viewport.right',
-    element: document.querySelector('.viewport.right'),
-    style: "translate3d(-200vw, 0vh, 0px)"
-  }
-};
-viewports.current = viewports.center;
-
-// Center well scroll listener
-var centerWellScrollListener = null;
-if(viewports.current === viewports.center) {
-  var logo = viewports.center.element.querySelector('.logo-container');
-  centerWellScrollListener = onScroll(viewports.center.element, logo);
-  viewports.center.element.addEventListener('scroll', centerWellScrollListener);
+var viewportContainer = $('#vpc');
+var currentViewport = $('.viewport.active');
+var currentViewportClass = 'center';
+if(currentViewport.hasClass('right')) {
+  currentViewportClass = 'right';
+} else if(currentViewport.hasClass('left')) {
+  currentViewportClass = 'left';
 }
 var originalPageState = {
   url: document.location.pathname,
-  container: viewports.current.querySelector
+  container: currentViewportClass
   };
 var footerForm = $('footer .form-container');
 
+/**
+ * Center well scroll listener
+ */
+var centerViewport = $('.viewport.center');
+var logo = centerViewport.find('.logo-container').get(0);
+var bgContainer = centerViewport.find('.background-container').get(0);
+var logoScrollListener = makeScrollTransformForElement(currentViewport.get(0), logo, 10);
+var backgroundScrollListener = makeScrollTransformForElement(currentViewport.get(0), bgContainer, -5);
+centerViewport.on('scroll', logoScrollListener)
+  .on('scroll', backgroundScrollListener);
+
+/**
+ * Scroll listener for visible/invisible
+ */
 $('body').on('scroll', debounce(function(event) {
   $('.js-pub-sub.js-pub-sub-scroll').each(function(i, ele) {
     if($(ele).visible(true)) {
@@ -81,122 +76,90 @@ $('body').on('scroll', debounce(function(event) {
   });
 }, 50));
 
-$('body').on('click', 'a', function(event) {
-  // The <a> element
-  var current = event.target;
+/**
+ * Viewport switching
+ */
+function swapViews(target) {
+  // Animate
+  $('body').addClass('animate');
+  var animationClass = currentViewportClass + '-to-' + target;
+  viewportContainer.addClass(animationClass);
+  viewportContainer.removeClass('center left right');
+  viewportContainer.addClass(target);
+  setTimeout(function() {
+    $('body').removeClass('animate');
+    viewportContainer.removeClass(animationClass);
+  }, 2000);
 
-  if(current.classList.contains('js-page-transition')) {
-    event.preventDefault();
-    var container = '',
-      targetContainer = viewports.center,
-      targetViewportContainerClass = 'center';
+  // 
+  currentViewport.removeClass('active');
+  currentViewport = $('.viewport.' + target);
+  currentViewport.addClass('active');
+  currentViewportClass = target;
 
-    if(current.classList.contains('js-page-destination-right')) {
-      targetContainer = viewports.right;
-      targetViewportContainerClass = 'right'
-    }
+  return currentViewport;
+}
 
-    viewportContainer.classList.remove('center', 'left', 'right');
-    viewportContainer.classList.add(targetViewportContainerClass);
-//    viewportContainer.style.transform = targetContainer.style;
-    container = targetContainer.querySelector;
-    viewports.current = targetContainer;
-
-    var newStateObject = {
-      url: current.getAttribute('href'),
-      container: targetContainer.querySelector
-      };
-
-    console.log('[AJAX LOAD]', $(viewports.current), current.getAttribute('href'), container);
-    console.log('pushState', newStateObject, current.getAttribute('data-title'), current.getAttribute('href'));
-    history.pushState(newStateObject, current.getAttribute('data-title'), current.getAttribute('href'));
-
+function fetchContent(url, targetViewportContainerClass) {
     try {
+      var context = {
+        className: targetViewportContainerClass
+      };
       $.ajax({
-        url: current.getAttribute('href'),
-        context: viewports.current.element,
-        success: function(data) {
+        url: url,
+        success: $.proxy(function(data) {
           var html = $(data);
-          var rightHtml = html.find(container);
+          var rightHtml = html.find('.viewport.' + this.className);
           var title = html.filter('title').text();
           document.title = title;
-          this.innerHTML = rightHtml.get(0).innerHTML;
+          currentViewport.innerHTML = rightHtml.get(0).innerHTML;
           moveFrame();
-
-          // Center well scroll listener
-          if(viewports.current === viewports.center) {
-            var logo = viewports.center.element.querySelector('.logo-container');
-            viewports.center.element.removeEventListener('scroll', centerWellScrollListener);
-            centerWellScrollListener = onScroll(viewports.center.element, logo);
-            viewports.center.element.addEventListener('scroll', centerWellScrollListener);
-          }
-        }
-      })
+        }, context)
+      });
     }
 
     catch(ex) {
       console.warn(ex.message);
       console.trace();
     }
+}
+
+/**
+ * Capture anchor clicks
+ */
+$('body').on('click', 'a', function(event) {
+  // The <a> element
+  var current = $(event.target);
+  if(current.hasClass('js-page-transition')) {
+    event.preventDefault();
+    var targetViewportContainerClass = 'center';
+    if(current.hasClass('js-page-destination-right')) {
+      targetViewportContainerClass = 'right'
+    }
+
+    swapViews(targetViewportContainerClass);
+    var targetTitle = current.attr('title')
+      , targetHref = current.attr('href')
+      , newStateObject = {
+        url: targetHref,
+        container: targetViewportContainerClass
+        };
+    history.pushState(newStateObject, targetTitle, targetHref);
+    fetchContent(targetHref, targetViewportContainerClass);
     return false;
-  } // END current.classList.contains('js-page-transition') check
+  }
 });
 
 window.onpopstate = function(event) {
   console.log('[POPSTATE]', event);
   var state = event.state || originalPageState;
-
-  try {
-    var container = state.container,
-      targetContainer = viewports.center,
-      targetViewportContainerClass = 'center';
-
-    if(container.indexOf('right') > -1) {
-      targetContainer = viewports.right;
-      targetViewportContainerClass = 'right';
-    }
-    
-    else if(container.indexOf('left') > -1) {
-      targetContainer = viewports.left;
-      targetViewportContainerClass = 'left';
-    }
-    
-    viewportContainer.classList.remove('center', 'left', 'right');
-    viewportContainer.classList.add(targetViewportContainerClass);
-//    viewportContainer.style.transform = targetContainer.style;
-    container = targetContainer.querySelector;
-    viewports.current = targetContainer;
-
-    $.ajax({
-      url: event.state.url,
-      context: document.querySelector(),
-      success: function(data) {
-        var html = $(data);
-        var rightHtml = html.find(container);
-        var title = html.filter('title').text();
-        document.title = title;
-        this.innerHTML = rightHtml.get(0).innerHTML;
-        moveFrame();
-
-        // Center well scroll listener
-        if(viewports.current === viewports.center) {
-          var logo = viewports.center.element.querySelector('.logo-container');
-          viewports.center.element.removeEventListener('scroll', centerWellScrollListener);
-          centerWellScrollListener = onScroll(viewports.center.element, logo);
-          viewports.center.element.addEventListener('scroll', centerWellScrollListener);
-        }
-      }
-    })
-  }
-
-  catch(ex) {
-    console.warn(ex.message);
-    console.trace();
-  }
+  var targetViewportContainerClass = state.container;
+  swapViews(targetViewportContainerClass);
+  fetchContent(event.state.url, targetViewportContainerClass);
 };
 
 function moveFrame() {
-  $(viewports.current.element).find('footer .form-container').html(footerForm.remove());
+  currentViewport.find('footer .form-container').html(footerForm.remove());
 }
 
 (function($) {
